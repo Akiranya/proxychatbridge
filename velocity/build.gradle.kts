c848ab4c39af874722f8e3e1d7043bc7e40f3f4e
Akiranya <porridgen@gmail.com>
2023-02-04 18:04:23 +0800

    Add dependency on LuckPerms

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 dependencies {
     compileOnly(project(":common"))
     compileOnly("com.github.Moulberry:adventure-binary-serializer:master-SNAPSHOT")
+    compileOnly("net.luckperms:api:5.4")
     compileOnly("com.velocitypowered", "velocity-api", "3.2.0-SNAPSHOT")
     annotationProcessor("com.velocitypowered", "velocity-api", "3.2.0-SNAPSHOT")
 }
